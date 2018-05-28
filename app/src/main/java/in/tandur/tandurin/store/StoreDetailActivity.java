a055e27e2afe3609be7ef7cef72512ac42762cbc
Jefry Dewangga <jefrydco@gmail.com>
2018-05-28 07:24:40 +0700

    Checkpoint.

---
+++ app/src/main/java/in/tandur/tandurin/store/StoreDetailActivity.java
@@ -0,0 +1,15 @@
+package in.tandur.tandurin.store;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import in.tandur.tandurin.R;
+
+public class StoreDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_store_detail);
+    }
+}
